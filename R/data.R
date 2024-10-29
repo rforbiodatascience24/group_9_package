b93e851f64d35cf2d04786f38192d2d789acdbca
AndreisMarco <andreismarco2002@gmail.com>
2024-10-29 09:42:56 +0100

    Added codon_code object

---
+++ R/data.R
@@ -0,0 +1,8 @@
+#' codon_code
+#'
+#' A named list where the each codon (three nucleotide sequence)
+#' is matched with the code amminoacid
+#'
+#'
+#' @source \url{https://www.ncbi.nlm.nih.gov/Taxonomy/Utils/wprintgc.cgi?chapter=tgencodes#SG1}
+"codon_code"
